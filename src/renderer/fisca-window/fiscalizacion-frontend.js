e9b87b7a5242cebc93efa50293d6dd472c4ad334
cristhianGigpz <cjacevedot@gmail.com>
2019-07-07 23:43:42 -0500

    join de los cambios

--- src/renderer/fisca-window/fiscalizacion-frontend.js
+++ src/renderer/fisca-window/fiscalizacion-frontend.js
@@ -1,8 +1,9 @@
-import { remote, ipcRenderer } from 'electron'
+import { remote } from 'electron'
+import { operacion, showDialog, limpiarInputs, pasarFocus } from './fisca-Window/util-fisca'
 
+let totFinal=0;
 window.addEventListener('load', () => {
   cancelButton()
-  
   //testConnection()
 })
 
@@ -15,11 +16,33 @@ function cancelButton () {
   })
 }
 
-function operacion(arancel, area, construccion, obrasComp, rustico = true){
-  if (rustico){
-    let resultado = ((arancel * area) + (construccion + obrasComp)) / 2
-  }else{
-    let resultado = ((arancel * area) + (construccion + obrasComp))
-  }
-  
-}
+
+document.getElementById('btnAgregar').addEventListener('click', function(event){
+  //showDialog('info', 'Aviso', 'Se ha agregado el registro')
+  let data = []
+  event.preventDefault()
+  const form = document.getElementById('frmAgregar')
+  //pasarFocus(form)
+  //if (validateForm(form)) {
+    let inputs = form.getElementsByTagName('input')
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == 'text' || (inputs[i].type == 'radio' && inputs[i].checked)){
+        if(inputs[i].value.trim() === "" && inputs[i].required === true) {
+              alert("Digite el valor " + ' del ' + " campo");
+              inputs[i].focus();
+              inputs[i].style.borderColor = "red";
+              inputs[i].style.borderStyle = "dashed";
+              return false;
+        }else{
+          data.push(inputs[i].value)
+        }
+      }
+    }
+    //let datos = data.toString().split(",");
+    //alert(datos[0]+' '+datos[1]+' '+datos[2]+' '+ datos[3]+ ' '+ datos[4])
+    if (data.length>0){
+      operacion(data[0],data[1],data[2],data[3],data[4])
+      limpiarInputs('frmAgregar', 'text')
+    }
+  //}
+})
---
+++ src/renderer/fisca-window/util-fisca.js
@@ -0,0 +1,74 @@
+import { ipcRenderer } from 'electron'
+
+function operacion(arancel, area, construccion, obrasComp, terreno = 'rustico'){
+    //alert(arancel+' '+area+ ' ' +construccion + ' ' +obrasComp + ' ' +terreno)
+    let resultado;
+    let totgeneral = document.getElementById('totTabla');
+    if (terreno == 'rustico'){
+      resultado = ((parseFloat(arancel) * parseFloat(area)) + (parseFloat(construccion) + parseFloat(obrasComp))) / 2
+      resultado = resultado.toFixed(2)
+    }
+    if (terreno == 'urbano'){
+      resultado = ((parseFloat(arancel) * parseFloat(area)) + (parseFloat(construccion) + parseFloat(obrasComp)))
+      resultado = resultado.toFixed(2)
+    }
+    totFinal+=parseInt(resultado)
+    crearFila(arancel, area, construccion, obrasComp, terreno, resultado)
+    //alert(resultado)
+    totgeneral.innerHTML = totFinal.toFixed(2)
+}
+
+function showDialog(type, title, msg){
+    ipcRenderer.send('show-dialog', {type: type, title: title, message: msg})
+}
+  
+function crearFila(arancel, area, construccion, obrasComp, terreno, total) {
+  var row= `
+  <tr>
+    <td>${arancel}</td>
+    <td>${area}</td>
+    <td>${construccion}</td>
+    <td>${obrasComp}</td>
+    <td>${terreno}</td>
+    <td>${total}</td>
+  </tr>
+  `;
+  let tabla= document.getElementById('destinoFila')
+  tabla.insertAdjacentHTML('afterbegin', row)
+}
+
+function limpiarInputs(elementoPadre, tipo){
+  let elemento = document.getElementById(elementoPadre)
+  //elemento = document.getElementById('frmAgregar')
+  let inputs = elemento.getElementsByTagName('input')
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == tipo){
+      inputs[i].value = ''
+    }
+  }
+  inputs[0].focus()
+}
+
+function pasarFocus(container){
+  const elemento = document.getElementById(container)
+  let inputs = elemento.querySelectorAll(".form-control")
+  alert(inputs.length)
+  /*for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type === 'text'){
+      inputs[i].addEventListener('keydown', inputCharacters);
+    } 
+  }*/
+}
+// function inputCharacters(event) {
+//   if (event.keyCode == 13) {
+//     document.getElementById('inputSecond').focus();
+//   }
+// }
+
+module.exports = {
+    crearFila,
+    showDialog,
+    operacion,
+    limpiarInputs,
+    pasarFocus
+}
